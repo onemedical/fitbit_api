f6b4faffcfa88e0a6e0d898775accf61504766f4
Ben Yee <ben.yee@onemedical.com>
2020-08-06 16:55:28 -0700

    updates for sleep api 1.2

--- lib/fitbit_api/sleep.rb
+++ lib/fitbit_api/sleep.rb
@@ -1,35 +1,21 @@
 module FitbitAPI
   class Client
-    SLEEP_RESOURCES = %w(startTime timeInBed minutesAsleep awakeningsCount
-                         minutesAwake minutesToFallAsleep minutesAfterWakeup efficiency)
-
-    def sleep_logs(date=Date.today, opts={})
+    # GET user/[user-id]/sleep/date/[date].json
+    def sleep_logs(date = Date.today, opts = {})
       get("user/#{user_id}/sleep/date/#{format_date(date)}.json", opts)
     end
 
-    def sleep_time_series(resource, opts={})
+    # GET user/[user-id]/sleep/date/[startDate]/[endDate].json
+    def sleep_time_series(opts = {})
       start_date = opts[:start_date]
       end_date   = opts[:end_date] || Date.today
-      period     = opts[:period]
 
-      unless SLEEP_RESOURCES.include?(resource)
-        raise FitbitAPI::InvalidArgumentError, "Invalid resource: \"#{resource}\". Please provide one of the following: #{SLEEP_RESOURCES}."
-      end
-
-      if [period, start_date].none?
+      if [start_date].none?
         raise FitbitAPI::InvalidArgumentError, 'A start_date or period is required.'
       end
 
-      if period && !PERIODS.include?(period)
-        raise FitbitAPI::InvalidArgumentError, "Invalid period: \"#{period}\". Please provide one of the following: #{PERIODS}."
-      end
+      result = get("user/#{user_id}/sleep/#{format_date(start_date)}/#{format_date(end_date)}.json", opts)
 
-      if period
-        result = get("user/#{user_id}/activities/#{resource}/date/#{format_date(end_date)}/#{period}.json", opts)
-      else
-        result = get("user/#{user_id}/activities/#{resource}/date/#{format_date(start_date)}/#{format_date(end_date)}.json", opts)
-      end
-      # remove root key from response
       result.values[0]
     end
   end
