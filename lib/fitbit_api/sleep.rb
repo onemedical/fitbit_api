b3fc3331e31be802060d3404609ea5e470c9e4dd
Ben Yee <ben.yee@onemedical.com>
2020-08-07 13:35:44 -0700

    update sleep api for time series

--- lib/fitbit_api/sleep.rb
+++ lib/fitbit_api/sleep.rb
@@ -1,13 +1,15 @@
 module FitbitAPI
   class Client
+    API_VERSION = '1.2'
     # GET user/[user-id]/sleep/date/[date].json
     def sleep_logs(date = Date.today, opts = {})
+      opts[:api_version] = API_VERSION
       get("user/#{user_id}/sleep/date/#{format_date(date)}.json", opts)
     end
 
     # GET user/[user-id]/sleep/date/[startDate]/[endDate].json
     def sleep_time_series(opts = {})
-      opts[:api_version] = "1.2"
+      opts[:api_version] = API_VERSION
       start_date = opts[:start_date]
       end_date   = opts[:end_date] || Date.today
 
@@ -15,7 +17,7 @@ module FitbitAPI
         raise FitbitAPI::InvalidArgumentError, 'A start_date or period is required.'
       end
 
-      result = get("user/#{user_id}/sleep/#{format_date(start_date)}/#{format_date(end_date)}.json", opts)
+      result = get("user/#{user_id}/sleep/date/#{format_date(start_date)}/#{format_date(end_date)}.json", opts)
 
       result.values[0]
     end
