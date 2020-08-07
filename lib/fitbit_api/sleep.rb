23b96c6094555a0f5e4c04df738dd88126ae4f29
Ben Yee <ben.yee@onemedical.com>
2020-08-07 14:10:18 -0700

    add sleep_log

--- lib/fitbit_api/sleep.rb
+++ lib/fitbit_api/sleep.rb
@@ -1,6 +1,7 @@
 module FitbitAPI
   class Client
     API_VERSION = '1.2'
+
     # GET user/[user-id]/sleep/date/[date].json
     def sleep_logs(date = Date.today, opts = {})
       opts[:api_version] = API_VERSION
@@ -18,6 +19,22 @@ module FitbitAPI
       end
 
       result = get("user/#{user_id}/sleep/date/#{format_date(start_date)}/#{format_date(end_date)}.json", opts)
+
+      result.values[0]
+    end
+
+    # POST https://api.fitbit.com/1.2/user/[user-id]/sleep.json
+    # Log Sleep Record
+    #
+    # ==== POST Parameters
+    # * +:startTime+ - activity start time; formatted in HH:mm:ss
+    # * +:duration+ - duration in milliseconds
+    # * +:date+ - log entry date; formatted in yyyy-MM-dd
+    #
+    def log_sleep(opts = {})
+      opts[:api_version] = API_VERSION
+
+      result = post("user/#{user_id}/sleep.json", opts)
 
       result.values[0]
     end
