b6805c91e90879be26e5ef871f0c44ecb3e7d0e8
Pamela Ocampo <pamela.ocampo@gmail.com>
2020-11-16 11:47:28 -0800

    Add coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fitbit_api.gemspec
 gemspec
+
+gem 'simplecov', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fitbit_api'
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  track_files '{app,lib}/**/*.rb'
+end
